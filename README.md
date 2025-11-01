1c8390c0f44fc00efb2f77bae8fe1b7add2d603d
ElJoker63 <eljoker630@gmail.com>
2025-11-01 16:30:23 -0500

    Init commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# QOD Slipstream Client for Termux
+
+This repository contains a script to install and set up the Slipstream Client on Termux.
+
+## Installation
+
+Run the following command in Termux to install:
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/ElJoker63/qod/main/termux-slipstream-client.sh | bash
+```
+
+This will:
+- Update and upgrade packages
+- Install required dependencies
+- Download and set up the slipstream-client
+- Create a `qod.sh` script for running the client
+- Clean up the installation script
+
+## Usage
+
+After installation, run the client with:
+
+```bash
+./qod.sh
---
+++ termux-slipstream-client.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+pkg install wget
+
+pkg update && pkg upgrade -y
+
+pkg install openssl
+
+pkg install termux-tools
+
+pkg update && pkg upgrade -y
+
+pkg install wget openssl
+
+pkg install dos2unix -y
+
+dos2unix termux-slipstream-client.sh
+
+pkg install git -y
+
+wget https://raw.githubusercontent.com/ElJoker63/qod/main/slipstream-client && chmod +x slipstream-client
+
+echo './slipstream-client --tcp-listen-port=5201 --resolver=81.30.156.72:5300 --domain=aewarevpn.ddns.net --keep-alive-interval=1500000 --congestion-control=cubic' > qod.sh
+
+chmod +x qod.sh
+
+rm termux-slipstream-client.sh
