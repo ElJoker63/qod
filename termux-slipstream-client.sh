9946d6fba95a5b254c2ca2f73456a792d4090759
ElJoker63 <eljoker630@gmail.com>
2025-11-01 19:48:07 -0500

    Update termux-slipstream-client.sh

--- termux-slipstream-client.sh
+++ termux-slipstream-client.sh
@@ -20,6 +20,6 @@ pkg install git -y
 
 wget https://raw.githubusercontent.com/ElJoker63/qod/main/slipstream-client && chmod +x slipstream-client
 
-echo './slipstream-client --tcp-listen-port=5201 --resolver=81.30.156.72:5300 --domain=aewarevpn.ddns.net --keep-alive-interval=1500000 --congestion-control=cubic' > qod.sh
+echo './slipstream-client --tcp-listen-port=5201 --resolver=81.30.156.72:5300 --domain=aewarevpn.ddns.net --keep-alive-interval=600 --congestion-control=cubic --gso=true' > qod.sh
 
 chmod +x qod.sh
