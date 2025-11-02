874cc77db9944b0ac790c64abeeb1780f0d7227e
ElJoker63 <eljoker630@gmail.com>
2025-11-01 21:29:33 -0500

    Update termux-slipstream-client.sh

--- termux-slipstream-client.sh
+++ termux-slipstream-client.sh
@@ -4,7 +4,7 @@ pkg update -y && pkg upgrade -y
 pkg install termux-tools -y
 pkg install wget openssl -y
 pkg install dos2unix -y
-dos2unix termux-slipstream-client.sh
 pkg install git -y
 wget https://raw.githubusercontent.com/ElJoker63/qod/main/slipstream-client && chmod +x slipstream-client
+dos2unix slipstream-client
 wget https://raw.githubusercontent.com/ElJoker63/qod/main/qod -O qod.sh && chmod +x qod.sh
