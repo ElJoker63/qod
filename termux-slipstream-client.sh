1f9d0dc36255812619e31d05c54f2b4cb25a2a9a
ElJoker63 <eljoker630@gmail.com>
2025-11-02 18:03:45 -0600

    .

--- termux-slipstream-client.sh
+++ termux-slipstream-client.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-pkg update -y && pkg upgrade -y
+pkg update && pkg upgrade -y
 pkg install termux-tools -y
-pkg install wget openssl -y
-pkg install dos2unix -y
+pkg install wget -y
+pkg install openssl -y
+pkg install curl -y
 pkg install git -y
+pkg install termux-services -y
+pkg install dos2unix -y
+pkg install iproute2 -y
 wget https://raw.githubusercontent.com/ElJoker63/qod/main/slipstream-client && chmod +x slipstream-client
 dos2unix slipstream-client
 wget https://raw.githubusercontent.com/ElJoker63/qod/main/qod -O qod.sh && chmod +x qod.sh
